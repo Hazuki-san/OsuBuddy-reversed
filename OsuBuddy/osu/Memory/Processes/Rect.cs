688e92f4a92b6c225f361ea53036595fa66d90b3
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2021-04-15 22:33:53 +0700

    [!] Add files

---
+++ OsuBuddy/osu/Memory/Processes/Rect.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+public struct Rect
+{
+	[CompilerGenerated]
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private int ALeftk__BackingField;
+
+	[CompilerGenerated]
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private int ATopk__BackingField;
+
+	[CompilerGenerated]
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private int ARightk__BackingField;
+
+	[CompilerGenerated]
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private int ABottomk__BackingField;
+
+	public int Left
+	{
+		[CompilerGenerated]
+		get
+		{
+			return ALeftk__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			ALeftk__BackingField = value;
+		}
+	}
+
+	public int Top
+	{
+		[CompilerGenerated]
+		get
+		{
+			return ATopk__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			ATopk__BackingField = value;
+		}
+	}
+
+	public int Right
+	{
+		[CompilerGenerated]
+		get
+		{
+			return ARightk__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			ARightk__BackingField = value;
+		}
+	}
+
+	public int Bottom
+	{
+		[CompilerGenerated]
+		get
+		{
+			return ABottomk__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			ABottomk__BackingField = value;
+		}
+	}
+}
