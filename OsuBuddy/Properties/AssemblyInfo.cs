109883a609f4a638281a7de3acadb3fb7dc1bdc7
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2021-04-16 11:03:01 +0700

    [!] Clean SevenZip conflict

--- OsuBuddy/Properties/AssemblyInfo.cs
+++ OsuBuddy/Properties/AssemblyInfo.cs
@@ -7,7 +7,9 @@ using System.Runtime.Versioning;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyTitle("OsuBuddy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,4 +20,7 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 [assembly: Guid("0c8aab57-edd4-4cbb-ba2a-70ba35f0cceb")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: TargetFramework(".NETFramework,Version=v4.7.2", FrameworkDisplayName = ".NET Framework 4.7.2")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[module: UnverifiableCode]
