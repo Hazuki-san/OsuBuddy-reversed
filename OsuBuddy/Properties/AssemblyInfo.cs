372b3861f4d4dd79618f1cc190b2f5bdd933f038
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2021-04-16 11:08:44 +0700

    [!] Nothing

--- OsuBuddy/Properties/AssemblyInfo.cs
+++ OsuBuddy/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.Versioning;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: CompilationRelaxations(8)]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("OsuBuddy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,10 +17,26 @@ using System.Security.Permissions;
 [assembly: AssemblyProduct("OsuBuddy")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0c8aab57-edd4-4cbb-ba2a-70ba35f0cceb")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: TargetFramework(".NETFramework,Version=v4.7.2", FrameworkDisplayName = ".NET Framework 4.7.2")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[module: UnverifiableCode]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
